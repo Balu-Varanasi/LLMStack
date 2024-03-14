d35af854b8ade9383c1e3ed23c5083d2cc6d725c
Bala Subrahmanyam Varanasi <vabasu@gmail.com>
2024-03-14 17:52:45 +0530

    feat: add a single SQLDataSource handler

--- llmstack/datasources/handlers/databases/sql.py
+++ llmstack/datasources/handlers/databases/sql.py
@@ -9,7 +9,11 @@ from llmstack.common.blocks.data.store.database.database_reader import (
     DatabaseReader,
     DatabaseReaderInput,
 )
-from llmstack.common.blocks.data.store.database.mysql import MySQLConfiguration
+from llmstack.common.blocks.data.store.database.utils import (
+    DATABASES,
+    DatabaseConfiguration,
+    DatabaseEngineType,
+)
 from llmstack.common.blocks.data.store.vectorstore import Document
 from llmstack.common.utils.models import Config
 from llmstack.datasources.handlers.datasource_processor import (
@@ -22,43 +26,54 @@ from llmstack.datasources.models import DataSource
 logger = logging.getLogger(__name__)
 
 
-class MySQLConnection(_Schema):
-    host: str = Field(description="Host of the MySQL instance")
+class SQLConnection(_Schema):
+    host: str = Field(description="Host of the Database instance")
     port: int = Field(
-        description="Port number to connect to the MySQL instance",
+        description="Port number to connect to the Database instance",
     )
-    database_name: str = Field(description="MySQL database name")
-    username: str = Field(description="MySQL username")
-    password: Optional[str] = Field(description="MySQL password")
+    database_name: str = Field(description="Database name")
+    username: str = Field(description="Database username")
+    password: Optional[str] = Field(description="Database password")
 
 
-class MySQLDatabaseSchema(DataSourceSchema):
-    connection: Optional[MySQLConnection] = Field(
-        description="MySQL connection details",
+class SQLDatabaseSchema(DataSourceSchema):
+    connection: Optional[SQLConnection] = Field(
+        description="Database connection details",
     )
 
 
-class MySQLConnectionConfiguration(Config):
-    config_type = "mysql_connection"
+class SQLConnectionConfiguration(Config):
+    engine: Optional[DatabaseEngineType] = None
+    config_type: Optional[str] = None
     is_encrypted = True
-    mysql_config: Optional[Dict]
+    config: Optional[Dict]
+
+    def __init__(self, engine: DatabaseEngineType, *args, **kwargs):
+        super().__init__(**args, **kwargs)
+        self.engine = engine
+        self.config_type = f"{engine}_connection"
 
 
-class MySQLDataSource(DataSourceProcessor[MySQLDatabaseSchema]):
+class SQLDataSource(DataSourceProcessor[SQLDatabaseSchema]):
     # Initializer for the class.
     # It requires a datasource object as input, checks if it has a 'data'
     # configuration, and sets up Weaviate Database Configuration.
     def __init__(self, datasource: DataSource):
         self.datasource = datasource
+
+        if self.datasource.type.slug not in DATABASES:
+            raise ValueError(f"Database engine {self.datasource.type.slug} not supported")
+
         if self.datasource.config and "data" in self.datasource.config:
-            config_dict = MySQLConnectionConfiguration().from_dict(
+            config_dict = SQLConnectionConfiguration(engine=self.datasource.type.slug).from_dict(
                 self.datasource.config,
                 self.datasource.profile.decrypt_value,
             )
-            self._configuration = MySQLDatabaseSchema(
-                **config_dict["mysql_config"],
+            self._configuration = SQLDatabaseSchema(
+                **config_dict["config"],
             )
-            self._reader_configuration = MySQLConfiguration(
+            self._reader_configuration = DatabaseConfiguration(
+                engine=self.database_engine,
                 user=self._configuration.connection.username,
                 password=self._configuration.connection.password,
                 host=self._configuration.connection.host,
@@ -68,37 +83,31 @@ class MySQLDataSource(DataSourceProcessor[MySQLDatabaseSchema]):
             )
             self._source_name = self.datasource.name
 
-    @staticmethod
-    def name() -> str:
-        return "MySQL"
+    def name(self) -> str:
+        return self.datasource.type.name
 
-    @staticmethod
-    def slug() -> str:
-        return "mysql"
+    def slug(self) -> str:
+        return self.datasource.type.slug
 
-    @staticmethod
-    def description() -> str:
-        return "Connect to a MySQL database"
+    def description(self) -> str:
+        return f"Connect to a {self.datasource.type.name} database"
 
     # This static method takes a dictionary for configuration and a DataSource object as inputs.
     # Validation of these inputs is performed and a dictionary containing the
-    # MySQL Connection Configuration is returned.
+    # Database Connection Configuration is returned.
     @staticmethod
     def process_validate_config(
         config_data: dict,
         datasource: DataSource,
     ) -> dict:
-        return MySQLConnectionConfiguration(
-            mysql_config=config_data,
+        return SQLConnectionConfiguration(
+            config=config_data,
         ).to_dict(
             encrypt_fn=datasource.profile.encrypt_value,
         )
 
-    # This static method returns the provider slug for the datasource
-    # connector.
-    @staticmethod
-    def provider_slug() -> str:
-        return "mysql"
+    def provider_slug(self) -> str:
+        return self.datasource.type.slug
 
     def validate_and_process(self, data: dict) -> List[DataSourceEntryItem]:
         raise NotImplementedError
@@ -110,9 +119,9 @@ class MySQLDataSource(DataSourceProcessor[MySQLDatabaseSchema]):
         raise NotImplementedError
 
     def similarity_search(self, query: str, **kwargs) -> List[dict]:
-        mysql_client = DatabaseReader()
+        client = DatabaseReader()
         result = (
-            mysql_client.process(
+            client.process(
                 DatabaseReaderInput(
                     sql=query,
                 ),
@@ -148,9 +157,9 @@ class MySQLDataSource(DataSourceProcessor[MySQLDatabaseSchema]):
         ]
 
     def hybrid_search(self, query: str, **kwargs) -> List[dict]:
-        mysql_client = DatabaseReader()
+        client = DatabaseReader()
         result = (
-            mysql_client.process(
+            client.process(
                 DatabaseReaderInput(
                     sql=query,
                 ),
