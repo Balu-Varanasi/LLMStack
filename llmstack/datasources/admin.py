475afdb104cff1793106a1e25dfaf2050b1401f8
Bala Subrahmanyam Varanasi <vabasu@gmail.com>
2024-03-29 09:45:28 +0530

    feat: register DataSourceEntryFiles model in django admin for testing

--- llmstack/datasources/admin.py
+++ llmstack/datasources/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
 
-from .models import DataSource, DataSourceEntry, DataSourceType
+from .models import DataSource, DataSourceEntry, DataSourceEntryFiles, DataSourceType
 
 
+@admin.register(DataSourceEntry)
 class DataSourceEntryAdmin(admin.ModelAdmin):
     search_fields = ["uuid", "name", "datasource__name"]
     list_display = ["name"]
 
 
+@admin.register(DataSource)
 class DataSourceAdmin(admin.ModelAdmin):
     search_fields = ["uuid", "owner__email"]
 
 
-admin.site.register(DataSourceEntry, DataSourceEntryAdmin)
-admin.site.register(DataSourceType, DataSourceAdmin)
-admin.site.register(DataSource)
+@admin.register(DataSourceType)
+class DataSourceTypeAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+
+
+@admin.register(DataSourceEntryFiles)
+class DataSourceEntryFilesAdmin(admin.ModelAdmin):
+    search_fields = ["uuid", "ref_id", "file"]
+    list_display = ["uuid", "ref_id", "file", "created_at"]
